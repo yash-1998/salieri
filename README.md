d0344942d6aa9be630a34dcc81acceeb0631c5f4
yash-1998 <eternalblizzard1998@gmail.com>
2018-10-20 11:33:45 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# salieri
+
+salieriexpensemanager
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Salieri",
+      home: new HomePage(),
+      theme: new ThemeData(
+          primarySwatch: Colors.blue
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sign In"),
+          elevation : 5.00,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: null ,
+              child: Text ("Sign In With Google"),
+              color: Colors.blueAccent,
+            )
+          ],
+        )
+    );
+  }
+}
+
