bd92ce3f9d93a440d548244990db82c18d6b3eeb
harshit2202 <harshitagarwal49@gmail.com>
2018-10-20 13:40:29 +0530

    Splash Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:salieri/SplashScreen.dart/';
+import 'package:salieri/SplashScreen.dart';
 import 'package:salieri/Login.dart';
 import 'package:salieri/Dashboard.dart';
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     Widget build(BuildContext context) {
         return MaterialApp(
             title: "Salieri",
-            home: new HomePage(),
+            home: SplashScreen(),
             theme: new ThemeData(
                 primarySwatch: Colors.teal,
                 brightness: Brightness.light,
@@ -21,30 +21,6 @@ class MyApp extends StatelessWidget {
                 '/login' : (context) => Login(),
                 '/dashboard' : (context) => Dashboard(),
             },
-        );
-    }
-}
-
-class HomePage extends StatelessWidget {
-
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text("Sign In"),
-                elevation : 5.00,
-            ),
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                    RaisedButton(
-                        onPressed: null ,
-                        child: Text ("Sign In With Google"),
-                        color: Colors.blueAccent,
-                    )
-                ],
-            )
         );
     }
 }
