f6037c94d647603a8c297b003fc06e4af03220ff
Dancing-Goose <prathmesh1998@yahoo.co.in>
2018-10-27 16:46:26 +0530

    Personal Expense Page

--- lib/personal.dart
+++ lib/personal.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 class personal extends StatefulWidget {
   static FirebaseApp _app;
   static FirebaseUser _user;
@@ -20,22 +20,59 @@ class _personalState extends State<personal> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100.0,
-      height: 100.0,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black38,
-          width: 2.0,
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Personal Expenses"),
+//        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
 
-        ),
-          boxShadow: [new BoxShadow(
-            color: Colors.black,
-            blurRadius: 20.0,
-          ),]
       ),
-      child: new Text('HELLO WOLRDD'),
+
+      body: Column(
+//        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                          _sumvalue(),
+                          style: TextStyle(
+                              fontFamily: "Impact",
+                              color: Colors.green,
+                              fontSize: 48.0,
+                          ),
+                      ),
+                      subtitle: Column(
+                        children: <Widget>[
+                          Text("Total Expendture"),
+                          //Date and Time
+                          Text("Since "+_prevmonth())
+                        ],
+                      ),
+
+
+                    )
+                  ],
+                ),
+              )
+
+        ],
+      ),
     );
+  }
+
+  String _sumvalue(){
+    int val = 2000;
+
+    String val1;
+    val1 = val.toString();
+    return val1;
+  }
+  String _prevmonth(){
+    var curdate = new DateTime.now();
+    var prevmonth = new DateTime(curdate.year,curdate.month-1,curdate.day);
+    var showdate = new DateFormat.yMMMMd().format(prevmonth);
+    return showdate;
   }
 }
 
